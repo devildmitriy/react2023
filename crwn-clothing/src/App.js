0d5d0fef3165a574c8698e9c88d2bdaf6df7697f
devildmitriy <42486244+devildmitriy@users.noreply.github.com>
2023-01-24 10:39:41 +0000

    separated components

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,43 +1,40 @@
+import Directory from "./components/directory/directory.component";
+
 const App = () => {
-  return (
-    <div className='categories-container'>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-    </div>
-  );
+  const categories = [
+    {
+      id: 1,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+      backgroundColor: "#d1c4e9",
+    },
+    {
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+      backgroundColor: "#c5cae9",
+    },
+    {
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+      backgroundColor: "#bbdefb",
+    },
+    {
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+      backgroundColor: "#b2ebf2",
+    },
+    {
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+      backgroundColor: "#b2dfdb",
+    },
+  ];
+
+  return <Directory categories={categories} />;
 };
 
 export default App;
---
+++ crwn-clothing/src/components/category-item/category-item.component.jsx
@@ -0,0 +1,21 @@
+import "./category-item.syles.scss";
+
+const CategoryItem = ({ category }) => {
+  const { title, imageUrl, backgroundColor } = category;
+
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        //style={{ backgroundImage: `url(${imageUrl})` }}
+        style={{ backgroundColor: backgroundColor }}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
